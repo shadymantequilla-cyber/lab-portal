4244ae19fd59b5d8081af360514bd6422071fc0c
shadymantequilla-cyber <shadymantequilla@gmail.com>
2026-05-24 16:20:13 -0500

    Refactor next.config.ts for TypeScript settings

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
 }
+
 module.exports = nextConfig
